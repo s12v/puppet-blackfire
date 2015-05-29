054adc094c8b0a88784233e079e09768aadd7197
Sergey Novikov <snov@snov.me>
2015-05-29 22:52:18 +0200

    puppetlabs-apt 2.0 support

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,13 +8,28 @@ class blackfire::repo inherits blackfire {
           class { 'apt': }
         }
 
-        apt::source { 'blackfire':
-          location    => 'http://packages.blackfire.io/debian',
-          release     => 'any',
-          repos       => 'main',
-          key         => 'D59097AB',
-          key_source  => 'https://packagecloud.io/gpg.key',
-          include_src => false,
+        if defined('apt::setting') {
+        # apt >= 2.0
+          apt::source { 'blackfire':
+            location => 'http://packages.blackfire.io/debian',
+            release  => 'any',
+            repos    => 'main',
+            key      => {
+              source => 'https://packagecloud.io/gpg.key',
+              id     => '418A7F2FB0E1E6E7EABF6FE8C2E73424D59097AB',
+            },
+          }
+          Exec['apt_update'] -> Package <||>
+        } else {
+          # apt >= 1.x
+          apt::source { 'blackfire':
+            location    => 'http://packages.blackfire.io/debian',
+            release     => 'any',
+            repos       => 'main',
+            key         => 'D59097AB',
+            key_source  => 'https://packagecloud.io/gpg.key',
+            include_src => false,
+          }
         }
       }
       'redhat': {

8df08c107a53f644bf7fde66454f376da72e1826
flepoutre <84913246+flepoutre@users.noreply.github.com>
2023-12-05 15:54:45 +0000

    Puppet8x support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,31 @@
 # Installs and configures Blackfire agent and PHP extension
+#
+# @param server_id
+#
+# @param server_token
+#
+# @param agent
+#
+# @param php
+#
+# @param manage_repo
+#
 class blackfire (
-  $server_id = $blackfire::params::server_id,
-  $server_token = $blackfire::params::server_token,
-  $agent = $blackfire::params::agent,
-  $php = $blackfire::params::php,
-  $manage_repo = $blackfire::params::manage_repo
+  Optional[String] $server_id    = $blackfire::params::server_id,
+  Optional[String] $server_token = $blackfire::params::server_token,
+  Hash $agent                    = $blackfire::params::agent,
+  Hash $php                      = $blackfire::params::php,
+  Boolean $manage_repo           = $blackfire::params::manage_repo
 ) inherits blackfire::params {
-
-  validate_string($server_id)
-  validate_string($server_token)
   if empty($server_id) or empty($server_token) {
     fail 'server_id and server_token are required. Get them on https://blackfire.io/account/agents'
   }
-  validate_hash($agent)
-  validate_hash($php)
-  validate_bool($manage_repo)
 
-  anchor { '::blackfire::begin': }
-  -> class { '::blackfire::repo': }
-  -> class { '::blackfire::agent': }
-  ~> class { '::blackfire::php': }
-  ~> anchor { '::blackfire::end': }
+  contain 'blackfire::repo'
+  contain 'blackfire::agent'
+  contain 'blackfire::php'
 
+  Class['blackfire::repo']
+  -> Class['blackfire::agent']
+  ~> Class['blackfire::php']
 }
